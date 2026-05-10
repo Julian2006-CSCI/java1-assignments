a54d6a840f26b9aaaabd2f0459b5c494e3a4ef78
Julian2006-CSCI <ninjajulian14@gmail.com>
2026-05-10 06:46:35 +0000

    Add HelloWorld program

---
+++ Car.java
@@ -0,0 +1,27 @@
+public class Car {
+    private int modelYear;
+    private int purchasePrice;
+    private int currentValue;
+
+    public void setModelYear(int year) {
+        modelYear = year;
+    }
+
+    public void setPurchasePrice(int price) {
+        purchasePrice = price;
+    }
+
+    public void calcCurrentValue(int currentYear) {
+        double deprectiationRate = 0.15;
+        int carAge = currentYear - modelYear;
+
+        currentValue = (int)(purchasePrice * Math.pow((1 - deprectiationRate), carAge));
+    }
+
+    public void printInfo() {
+        System.out.println("Car's information:");
+        System.out.println("  Model year: " + modelYear);
+        System.out.println("  Purchase price: $" + purchasePrice);
+        System.out.println("  Current value: $" + currentValue);
+    }
+}
